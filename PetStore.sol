d4a88b283cb042eb16204b9dadc2111bb1220b16
Xin Chen <t-chx@microsoft.com>
2020-11-07 22:22:59 +0800

    Update PetStore.sol

--- PetStore.sol
+++ PetStore.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.4.25;
     // seller(出售人：将宠物上架、制定价格、售卖成功则收款)
     address public seller;
     // buyer (购买人：选择宠物、进行购买)
-    address[16] public buyer;
+    address public buyer;
     
     struct pet {
         uint Id;//ID 
@@ -20,28 +20,35 @@ pragma solidity ^0.4.25;
         uint birth;//出生日期
         uint value;//价格
         string description;//描述
-        address delegate;
-        uint amount;//数量
+        address owner;
     }
+    
+    pet[88] pets;//假设宠物数量为88
 
     mapping (address => uint256) public balances; 
     
-    function BuyPet(uint petId) public returns (uint) {
-        buyer[petId] = msg.sender;
-        return petId;
+    function BuyPet(uint Id) public returns (uint) {
+        require(Id >= 0 && Id <= 87);
+        //pets[Id] = msg.sender;
+        return Id;
     }
     
-    function _transfer(address _from, address _to, uint _value) internal {
-
-        require(_to != 0x0); 
-        require(balances[_from] >= _value); 
-        require(balances[_to] + _value > balances[_to]); //_value不能为负值
-
-        uint previousBalances = balances[_from] + balances[_to]; 
-
-        balances[_from] -= _value; 
-        balances[_to] += _value;
-
-        assert(balances[_from] + balances[_to] == previousBalances);  
+    function changeOwner(uint Id) {
+        pets[Id].owner = buyer;
     }
+  
+    function _transfer(uint _value) internal {
+
+        require(buyer != 0x0); 
+        require(balances[seller] >= _value); 
+        require(balances[buyer] + _value > balances[buyer]); //_value不能为负值
+
+        uint previousBalances = balances[seller] + balances[buyer]; 
+
+        balances[seller] -= _value; 
+        balances[buyer] += _value;
+
+        assert(balances[seller] + balances[buyer] == previousBalances);  
+    }
+  
  }
